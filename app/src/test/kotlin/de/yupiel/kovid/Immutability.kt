63330317e8e626c8a6109fdd93e7a7a4503e10b3
Yupiel <stefan_mitrovic@outlook.com>
2021-02-08 09:56:30 +0100

    Updated NullSafety and Immutability #1 #2

--- app/src/test/kotlin/de/yupiel/kovid/Immutability.kt
+++ app/src/test/kotlin/de/yupiel/kovid/Immutability.kt
@@ -1,36 +1,52 @@
 package de.yupiel.kovid
 
+import com.google.common.collect.ImmutableList
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.assertThrows
+import java.lang.UnsupportedOperationException
 
 //These Tests don't serve a real purpose in this software and are more to showcase specific characteristics of the language
+@Disabled
 class Immutability {
     //Val variables are readonly after assignment, representing a constant from Java (final keyword)
 
+    data class DataClass(var value: String)
     @Test
-    fun reassignValueInValClassInstanceShouldBeEqual(){
-        data class DataClass(var value: String)
+    fun `Reassigning Var within Val Should be Equal`(){
         val test = DataClass("Test")
-        test.value = "yeet"                 //This works since the value parameter in the data class is a var
-                                                //it is not possible to reassign the val itself
-        assertEquals(test, DataClass("yeet"))
+        val actual = `Determine new Test Value`(test)       //This works since the value parameter in the data class is a var
+                                                                //it is not possible to reassign the val itself
+        assertEquals(DataClass("New Value"), actual)
+    }
+    fun `Determine new Test Value`(input: DataClass): String{
+        input.value = "New Value"
+        return input.value
     }
 
     @Test
-    fun emulateValWithVarShouldBeEqual(){
-        class TestClass(default: String){
-            var value: String = default
-                private set
-        }
+    fun `Int is an immutable class`(){
+        var a: Int = 10
+        var b: Int = 9
+
+        assertFalse(a === a.plus(b))
+    }
+
+    class TestClass(default: String){
+        var value: String = default
+            private set
+    }
+    @Test
+    fun `Non-Mutable Variable Should be Equal`(){
         var test = TestClass("Default Text")     //Reassignment of the value variable is now not possible anymore from the outside
                                                                     //however one could simply do the following
         test = TestClass("New Text")
 
-        assertEquals(test.value, "New Text")
+        assertEquals("New Text", test.value)
     }
 
-    sealed class Reassignable(default: String){
+    open class Reassignable(default: String){
         open val value: String = default
 
         class OverriddenReassignable(default: String): Reassignable(default){
@@ -38,12 +54,48 @@ class Immutability {
         }
     }
     @Test
-    fun overrideValWithVarShouldBeNotEqual(){
-        val test: Reassignable.OverriddenReassignable = Reassignable.OverriddenReassignable("Default Text")
-        val default: String = test.value
+    fun `Override Val with Var Should be Equal`(){
+        val testOverrideReassignable: Reassignable.OverriddenReassignable = Reassignable.OverriddenReassignable("Default Text")
 
-        test.value = "New Text"
+        testOverrideReassignable.value = "New Text"
 
-        assertNotEquals(test.value, default)
+        assertEquals("New Text", testOverrideReassignable.value)
+
+        val testReassignable: Reassignable = Reassignable("Default Text")
+        //testReassignable.value = "New Text"           This will not work since "value" in this case is a val
     }
+
+    @Test
+    fun `Collections Not Immutable but Non-Mutable by default`(){
+        val nonMutableList: List<Int> = listOf(1, 2, 3, 4, 5)
+        //[1] = 10    this would throw an error at compile time since there is no method to change the value available, however
+        (nonMutableList as MutableList)[1] = 10 //this is possible and valid as the list isn't immutable but non-mutable, this won't throw an compiler error or exception
+        assertEquals(10, nonMutableList[1])
+
+        val immutableList: ImmutableList<Int> = ImmutableList.of(1, 2, 3, 4, 5)
+        assertThrows<UnsupportedOperationException> { (immutableList as MutableList<Int>)[1] = 10 }
+        //This however does throw an Exception
+    }
+
+    companion object{
+        const val foobar: String = "irgendnenwert"
+        //const val staticDataClass: DataClass = DataClass("static")    //Only primitives and String allowed
+    }
+
+    data class NewDataClass(val value: String)
+    class StandardClass(val value: String)
+
+    @Test
+    fun `Demonstrate equality capabilities`(){
+        //val standardClass: StandardClass = StandardClass("Default Value")
+        //val standardClassTwo: StandardClass = StandardClass("Default Value")
+
+        //assertEquals(standardClass, standardClassTwo)
+
+        val dataClass: NewDataClass = NewDataClass("Default Value")
+        val dataClassTwo: NewDataClass = NewDataClass("Default Value")
+
+        assertEquals(dataClass, dataClassTwo)
+    }
+
 }
--- app/src/test/kotlin/de/yupiel/kovid/NullSafety.kt
+++ app/src/test/kotlin/de/yupiel/kovid/NullSafety.kt
@@ -3,55 +3,57 @@ package de.yupiel.kovid
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Disabled
 import java.lang.NullPointerException
 
 //These Tests don't serve a real purpose in this software and are more to showcase specific characteristics of the language
+@Disabled
 class NullSafety {
     @Test
-    fun questionMarkOperatorShouldBeNotNull(){
+    fun `Question mark operator should not be null`(){
         val testingClass: NullSafetyTestClass? = NullSafetyTestClass()  //? adds nullability to type
         assertNotNull(testingClass?.test)   //It is not possible to access nullable types with just a .
     }
     @Test
-    fun questionMarkOperatorShouldBeNull() {
+    fun `Question mark operator should be null`() {
         val testingClass: NullSafetyTestClass? = null   //Would usually throw an error at compile time
         assertNull(testingClass?.test)                      // ?. operator represents a safe call,
                                                             // returning null if b is null instead of throwing an error
     }
 
     @Test
-    fun doubleExclamationMarkOperatorShouldBeNotNull() {
+    fun `Double exclamation mark operator should not be null`() {
         val testingClass: NullSafetyTestClass? = NullSafetyTestClass()
         assertNotNull(testingClass!!.test)      //Converts the NullSafetyTestClass? into a non-nullable NullSafetyTestClass
     }
     @Test
-    fun doubleExclamationMarkOperatorShouldThrowException() {
+    fun `Double exclamation mark operator should be throw exception`() {
         val testingClass: NullSafetyTestClass? = null
         assertThrows<NullPointerException> { testingClass!!.test }
     }
 
     @Test
-    fun safeCastShouldBeNull() {
+    fun `Safe cast should be null`() {
         val testingClassProto: Any? = null
         val testingClass = testingClassProto as? NullSafetyTestClass  //Cast isn't really needed in this case
         assertNull(testingClass)                     //Safe cast will cast the object/variable if possible, if a ClassCastException occurs
                                                             //it will set the result to null
     }
     @Test
-    fun safeCastShouldBeNotNull() {
+    fun `Safe cast should not be null`() {
         val testingClassProto: Any? = NullSafetyTestClass()             //Object gotten from random library
         val testingClass = testingClassProto as? NullSafetyTestClass    //Safe cast in case the Object we got is actually null
         assertNotNull(testingClass)                                         //cast would throw exception if object was null without safe cast
     }
 
     @Test
-    fun elvisOperatorShouldBeNotNull(){
+    fun `Elvis operator should be true`(){
         val testingClass: NullSafetyTestClass? = NullSafetyTestClass()  //Elvis operator will return the left side if it is not null
         assertEquals(NullSafetyTestClass::class, (testingClass ?: 12)::class)   //and the right side if the left one is null
         assertTrue((testingClass ?: 12) is ParentClass)
     }
     @Test
-    fun elvisOperatorShouldBeNull() {
+    fun `Elvis operator should be equal`() {
         val testingClass: NullSafetyTestClass? = null
         val actual = testingClass ?: 12
         assertEquals(12, actual)
@@ -81,7 +83,7 @@ class NullSafety {
     }
 
     @Test
-    fun filterNullValuesInNullableCollectionsShouldBeTrue(){
+    fun `Filter Null values in collection of nullables should be true`(){
         val testingList: List<Int?> = listOf(1, 2, null, 4, 5, null, null, 6, 7, 8, 9)
 
         var expected: Boolean = true;
